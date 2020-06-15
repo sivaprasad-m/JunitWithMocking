94196c5252d911b4bb245e7e3a5984fb4c574725
MVS PRASAD <venkatasiva043@gmail.com>
2020-06-15 10:21:00 +0530

    initial commit

---
+++ src/main/java/com/ashokit/dao/ContactDao.java
@@ -0,0 +1,7 @@
+package com.ashokit.dao;
+
+public interface ContactDao {
+	
+	public String findEmailById(Integer id);
+
+}
---
+++ src/main/java/com/ashokit/service/ContactService.java
@@ -0,0 +1,7 @@
+package com.ashokit.service;
+
+public interface ContactService {
+	
+	public String findEmailById(Integer id);
+
+}
